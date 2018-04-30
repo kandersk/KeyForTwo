497d51b86abee1370ffc10b23c987497d9641969
Kristiāns Kanders <kristianskanders@Kristianss-MacBook-Pro.local>
2018-04-30 16:59:29 -0400

    Let's get this started

---
+++ logger.c
@@ -0,0 +1,9 @@
+/*  Kristians Kanders
+*   Keylogger software for Mac OS 
+*
+*   TODO:
+*     - Find propper mac os libraries for listening to key inputs
+*     - Implement a script that prints out a simple input to the terminal
+*     - Change script to log that key to a file
+*     - Finalize project that reads all keystrokes
+*/
